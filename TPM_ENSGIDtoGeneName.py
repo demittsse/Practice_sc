0baf206f88391e918ad5b2cd679f4a379338c7a7
demittsse <somrsaltt@gmail.com>
2019-10-16 14:29:57 +0900

    Update TPM_ENSGIDtoGeneName.py

--- TPM_ENSGIDtoGeneName.py
+++ TPM_ENSGIDtoGeneName.py
@@ -5,18 +5,14 @@ def ENSG2GeneName(inputTPM,outputFile):
 	for line in gtfline:
 		if "gene_name" in line:
 			print line.split('"')[5]
-			geneD[line.split('"')[1]]=line.split('"')[5]
-		
+			geneD[line.split('"')[1]]=line.split('"')[5]		
 	inf1=open("%s"%(str(inputTPM)))
 	resline=inf1.readlines()
 	inf1.close(); gtf.close()
-
 	ouf=open("%s"%(str(outputFile)),"w")
-
 	for rline in resline:
 		rkey=rline.split(",")[0].replace('"','')
 		geneN = geneD.get(rkey)
 		geneNa = str(geneN)+"_"+rkey
 		ouf.write('"%s",%s'%(geneNa, ",".join(rline.split(",")[1:])))
-
 	ouf.close()
