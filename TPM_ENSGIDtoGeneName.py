82d62dbd7a41026ffc7e63cb5728bf3c98931f17
demittsse <somrsaltt@gmail.com>
2019-10-16 11:00:12 +0900

    Create TPM_ENSGIDtoGeneName.py

---
+++ TPM_ENSGIDtoGeneName.py
@@ -0,0 +1,23 @@
+def ENSG2GeneName(inputTPM,outputFile):
+	gtf=open("/media/desktop-bi-16/D2C67EE7C67ECAED/BI/02ref/EnsHGmod.txt")
+	gtfline=gtf.readlines()
+	geneD={}
+	for line in gtfline:
+		if "gene_name" in line:
+			print line.split('"')[5]
+			geneD[line.split('"')[1]]=line.split('"')[5]
+		
+	inf1=open("%s"%(str(inputTPM)))
+	inf1=open("/media/desktop-bi-16/D2C67EE7C67ECAED/BI/07salmon/03plate/results/191015BY_salmonCounts.csv")
+	resline=inf1.readlines()
+	inf1.close(); gtf.close()
+
+	ouf=open("%s"%(str(outputFile)),"w")
+
+	for rline in resline:
+		rkey=rline.split(",")[0].replace('"','')
+		geneN = geneD.get(rkey)
+		geneNa = str(geneN)+"_"+rkey
+		ouf.write('"%s",%s'%(geneNa, ",".join(rline.split(",")[1:])))
+
+	ouf.close()
