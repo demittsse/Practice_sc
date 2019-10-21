259a513bf15d2645928d93f0f20bffc1216c7898
demittsse <somrsaltt@gmail.com>
2019-10-21 14:48:01 +0900

    Update TPM_ENSGIDtoGeneName.py

--- TPM_ENSGIDtoGeneName.py
+++ TPM_ENSGIDtoGeneName.py
@@ -16,3 +16,24 @@ def ENSG2GeneName(inputTPM,outputFile):
 		geneNa = str(geneN)+"_"+rkey
 		ouf.write('"%s",%s'%(geneNa, ",".join(rline.split(",")[1:])))
 	ouf.close()
+
+	
+##########################################
+def ENSG2GeneName(inputTPM,outputFile):
+	gtf=open("/home/cytogen-bi2/02ref/EnsHGmod.txt")
+	gtfline=gtf.readlines()
+	geneD={}
+	for line in gtfline:
+		if "gene_name" in line:
+			print line.split('"')[5]
+			geneD[line.split('"')[1]]=line.split('"')[5]		
+	inf1=open("%s"%(str(inputTPM)))
+	resline=inf1.readlines()
+	inf1.close(); gtf.close()
+	ouf=open("%s"%(str(outputFile)),"w")
+	for rline in resline:
+		rkey=rline.split(",")[0].replace('"','')
+		geneN = geneD.get(rkey)
+		geneNa = str(geneN)+"_"+rkey
+		ouf.write('"%s",%s'%(geneNa, ",".join(rline.split(",")[1:])))
+	ouf.close()
