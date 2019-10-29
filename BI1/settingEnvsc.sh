6c2483e41319a8cf2559a2ec0abbe1a4c05f8503
demittsse <somrsaltt@gmail.com>
2019-10-29 15:50:31 +0900

    Update settingEnvsc.sh

--- BI1/settingEnvsc.sh
+++ BI1/settingEnvsc.sh
@@ -2,3 +2,14 @@
 conda install -c bioconda bioconductor-sva
 conda create -n sva bioconductor-sva r-seurat
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+df1<-read.csv("/media/desktop-bi-16/D2C67EE7C67ECAED/BI/07salmon/04cmcpbmc/results/191016CMCPBMC_salmonTPM.csv")
+
+df3=df2[2:58]
+df4=df2[59:98]
+df3["batch"]=1
+df4["batch"]=2
+
+df5 = cbind(df3,df4)
