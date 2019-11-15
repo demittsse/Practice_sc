32234b35a7921a974bd20c41f1639db92f322536
demittsse <somrsaltt@gmail.com>
2019-11-15 15:29:27 +0900

    Update PC2_make_runStarRSEMGRCh38.97.py

--- BI_left/python/PC2_make_runStarRSEMGRCh38.97.py
+++ BI_left/python/PC2_make_runStarRSEMGRCh38.97.py
@@ -1,3 +1,4 @@
+
 #! Edited 2019.11.15
 #================================================================================================================
 ## trimming & Star mapping ENSEMBL 38.97 # samtools 1.9
@@ -5,7 +6,7 @@
 ##fastq.gz
 import os, glob, time
 start=time.strftime("%y%m%d")
-project_name="06ENS3802_%s"%(start)
+project_name="06ENS3803_%s"%(start)
 
 
 ### import input file ###
@@ -16,16 +17,16 @@ infFa=glob.glob("/home/cytogen-bi2/01raw/191001_CMC018_019_24eaPairedEnd_48fastq
 
 #trimmed
 
-trim1=glob.glob("/home/cytogen-bi2/03trim/01CMC1113/*_1_val_1.fq.gz")
-print len(trim1)
-trim2=glob.glob("/home/cytogen-bi2/03trim/02pbmc/*_1_val_1.fq.gz")
-print len(trim2)
-trim3=glob.glob("/home/cytogen-bi2/03trim/03H69plate/*_1_val_1.fq.gz")
-print len(trim3)
-trim4=glob.glob("/home/cytogen-bi2/03trim/04CMC1819/*_1_val_1.fq.gz")
-print len(trim4)
-trim5=glob.glob("/home/cytogen-bi2/03trim/03H69C1/*_1_val_1.fq.gz")
-print len(trim5)
+trim1=glob.glob("/dt1/03trim/01CMC1113/*_1_val_1.fq.gz")
+print(len(trim1))
+trim2=glob.glob("/dt1/03trim/02pbmc/*_1_val_1.fq.gz")
+print(len(trim2))
+trim3=glob.glob("/dt1/03trim/03H69plate/*_1_val_1.fq.gz")
+print(len(trim3))
+trim4=glob.glob("/dt1/03trim/04CMC1819/*_1_val_1.fq.gz")
+print(len(trim4))
+trim5=glob.glob("/dt1/03trim/03H69C1/*_1_val_1.fq.gz")
+print(len(trim5))
 
 ## check every file processed for each step ; check "/" after path
 rawfile_Extension="_1.fastq"
@@ -52,8 +53,8 @@ compRaw(1,trimFa, "_1_val_1.fq.gz", "/dt2/04rsem/06ENS3803_191111/", "scQuant.ge
 
 
 trimFa=trim1+trim2+trim3+trim4+trim5
-print "trimFa length = ", len(trimFa)
-print "sum of trim1...trimN = ", len(trim1)+len(trim2)+len(trim3)+len(trim4)+len(trim5)
+print("trimFa length = ", len(trimFa))
+print("sum of trim1...trimN = ", len(trim1)+len(trim2)+len(trim3)+len(trim4)+len(trim5))
 ### Create output folder & executable bash file ###
 
 #os.system("chmod 777 /home/cytogen-bi2/00script/BI_left/run/%sTrimStar%s.sh"%(start,project_name))
@@ -62,6 +63,7 @@ newTrimDir="%s02trim/%s"%(maindir,project_name)
 newStarDir="%s03star/%s"%(maindir,project_name)
 newQualiDir="%s03qualimap/%s"%(maindir,project_name)
 newRsemDir="%s04rsem/%s"%(maindir,project_name)
+newLogDir="%s04rsem/log_%s"%(maindir,project_name)
 #newQuantDir="/home/cytogen-bi2/04star_salmon/%s"%(project_name)
 
 ##make trim directory
@@ -77,12 +79,15 @@ if os.path.isdir(newQualiDir) == 0:
 if os.path.isdir(newRsemDir) == 0:
 	os.system("mkdir %s"%(newRsemDir))
 
+if os.path.isdir(newLogDir) == 0:
+	os.system("mkdir %s"%(newLogDir))
+
 
 #================================================================================================================
 ##Create ouput file
 #================================================================================================================
 
-ouf=open("/home/cytogenbi2/00script/BI_left/run/%s%s.sh"%(start,project_name),"w")
+ouf=open("/home/cytogenbi2/00script/BI_left/run/qualimap%s.sh"%(project_name),"w")
 # conda activate scrna
 
 #================================================================================================================
@@ -95,34 +100,44 @@ newQualiDir="%s03qualimap/%s"%(maindir,project_name)
 newRsemDir="%s04rsem/%s"%(maindir,project_name)
 
 genomeDir=maindir+"/00ref/STAR38" 
-sjdbGTFfile=maindir+"/00ref/ensembl/GRCh38/Homo_sapiens.GRCh38.97.gtf"
+sjdbGTFfile=maindir+"00ref/ensembl/GRCh38/Homo_sapiens.GRCh38.97.gtf"
 rsemrefDir=maindir+"00ref/STAR38/RSEM"
+newStarDir="/dt2/03star/06ENS3803_191111"
 
 lineNum=0
 for infile in trimFa:
 	lineNum+=1
 	if lineNum == 1: continue
-	print "#",lineNum	
+	print("#",lineNum)	
 	trfq2 = infile.replace("_1_val_1.fq.gz","_2_val_2.fq.gz")
 	sample = infile.split("/")[-1].split("_1_val_1.fq.gz")[0]
-	star_out_file="%s/%s/%s"%(newStarDir,project_name, sample)
-	quali_out_dir="%s/%s/%s"%(newQualiDir,project_name, sample)
-	rsem_out_file="%s/%s/%s"%(newRsemDir,project_name, sample)
+	star_out_file="%s/%s"%(newStarDir, sample)
+	quali_out_dir="%s/%s"%(newQualiDir, sample)
+	rsem_out_file="%s/%s"%(newRsemDir, sample)
 	cm_trimmed_star="STAR --runThreadN 2 --genomeDir %s --sjdbGTFfile %s --sjdbOverhang 150 --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.02 --alignIntronMin 20 --outFileNamePrefix %s --alignIntronMax 1000000 --alignMatesGapMax 1000000 --readFilesIn %s,%s --readFilesCommand zcat --quantMode TranscriptomeSAM\n\n\n"%(str(genomeDir), str(sjdbGTFfile), star_out_file, infile, trfq2)
 	cm_qualimap="qualimap rnaseq -outdir %s -a proportional -bam %sAligned.out.sam -gtf %s --java-mem-size=8G \n\n\n"%(quali_out_dir, star_out_file,sjdbGTFfile)
-	cm_rsem="rsem-calculate-expression --bam --no-bam-output --single-cell-prior -p 1 %sAligned.toTranscriptome.out.bam %s %sscQuant> %scrsem.log\n\n\n"%(star_out_file, str(rsemrefDir), rsem_out_file, rsem_out_file)
-	print(cm_trimmed_star)
+	cm_rsem="rsem-calculate-expression --bam --no-bam-output --single-cell-prior -p 1 %sAligned.toTranscriptome.out.bam %s %sscQuant> %sscrsem.log\n\n\n"%(star_out_file, str(rsemrefDir), rsem_out_file, rsem_out_file)
+	#print(cm_trimmed_star)
 	print(cm_qualimap)
-	print(cm_rsem)
-
-	ouf.write(cm_trimmed_star)
-	ouf.write("echo %s %s star alignment done!\n"%(project_name, sample))
+	#print(cm_rsem)
+	#ouf.write(cm_trimmed_star)
+	#ouf.write("echo %s %s star alignment done!\n"%(project_name, sample))
 	ouf.write(cm_qualimap)
 	ouf.write("echo %s %s Qualimap done!\n"%(project_name, sample))
-	ouf.write(cm_rsem)
-	ouf.write("echo %s %s rsem done!\n"%(project_name, sample))
+	#ouf.write(cm_rsem)
+	#ouf.write("echo %s %s rsem done!\n"%(project_name, sample))
 
 ouf.close()
+
+os.system("chmod 755 /home/cytogenbi2/00script/BI_left/run/qualimap%s.sh"%(project_name))
+
+print("tail /home/cytogenbi2/00script/BI_left/run/qualimap%s.sh"%(project_name))
+print("/home/cytogenbi2/00script/BI_left/run/./%squalimap%s.sh>%s/%sTrimStar.log"%(start,project_name,newLogDir, project_name))
+
+print("tail -f %s/%sTrimStar.log"%(newLogDir,project_name))
+
+
+
 
 #================================================================================================================
 ## Find "ALL DONE!"
