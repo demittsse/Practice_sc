bf998d75eb0e2aab1d21600c685c04841612f6ee
demittsse <somrsaltt@gmail.com>
2019-12-09 11:08:41 +0900

    Update 191202rhdf5.R

--- BI_left/R/191202rhdf5.R
+++ BI_left/R/191202rhdf5.R
@@ -3,25 +3,18 @@ Parsing H5 file
 Scripts to extract tab separated gene expression files can be created through the graphical user interface of ARCHS4. The script has to be executed as an R-script. A free version of R can be downloaded from: www.rstudio.com. Upon execution the script should install all required dependencies, and then download the full gene expression file before extracting the selected samples.
 
 
-library("rhdf5")
-library("preprocessCore")
 
-base_dir <- "/media/cytogenbi2/6eaf3ba8-a866-4e8a-97ef-23c61f7da612/BreastCancer/data/ReCount2OverARCHS4"
-setwd(base_dir)
-samples <- list.files(base_dir)
 
 destination_file = "/media/cytogenbi2/6eaf3ba8-a866-4e8a-97ef-23c61f7da612/BreastCancer/data/ReCount2OverARCHS4/GTEx_matrix.h5"
 extracted_expression_file = "GTEx_expression_matrix.tsv"
 
 
-destination_file = "/media/cytogenbi2/6eaf3ba8-a866-4e8a-97ef-23c61f7da612/BreastCancer/data/ReCount2OverARCHS4/TCGA_matrix.h5"
 extracted_expression_file = "TCGA_expression_matrix.tsv"
 
 
 # Retrieve information from compressed data
 h5ls(destination_file)
 
-genes = h5read(destination_file, "meta/genes")
 Tsamplid=h5read(destination_file, "/meta/sampleid")
 #> head(Tsamples)
 #[1] "3DFF72D2-F292-497E-ACE3-6FAA9C884205"
@@ -33,57 +26,75 @@ Tinfo=h5read(destination_file,"/info")
 Tcancertype=h5read(destination_file,"/meta/cancertype")
 #Breast Invasive Carcinoma 1246
 Tcaseid=h5read(destination_file,"/meta/gdc_cases.case_id")
-> head(Tcaseid)
-[1] "0004d251-3f70-4395-b175-c94c2f5b1b81"
-[2] "000d566c-96c7-4f1c-b36e-fa2222467983"
+#> head(Tcaseid)
+#[1] "0004d251-3f70-4395-b175-c94c2f5b1b81"
+#[2] "000d566c-96c7-4f1c-b36e-fa2222467983"
 
-#TCGA Barcode
 submitter_id=h5read(destination_file,"/meta/gdc_cases.samples.submitter_id")
-/meta/gdc_cases.samples.submitter_id
-[1] "TCGA-DD-AAVP-01A" "TCGA-KK-A7B2-01A" "TCGA-DC-6158-01A" "TCGA-DD-A4NP-01A"
-[5] "TCGA-HQ-A5ND-01A" "TCGA-HT-A614-01A"
+#/meta/gdc_cases.samples.submitter_id
+#[1] "TCGA-DD-AAVP-01A" "TCGA-KK-A7B2-01A" "TCGA-DC-6158-01A" "TCGA-DD-A4NP-01A"
+#[5] "TCGA-HQ-A5ND-01A" "TCGA-HT-A614-01A"
 sample_id=h5read(destination_file,"/meta/gdc_cases.samples.sample_id")
-> head(sample_id)
-[1] "9259e9ee-7279-4b62-8512-509cb705029c"
-[2] "bccb7f9d-8dde-45b7-a50d-6c7233239ceb"
+#> head(sample_id)
+#[1] "9259e9ee-7279-4b62-8512-509cb705029c"
+#[2] "bccb7f9d-8dde-45b7-a50d-6c7233239ceb"
 
+
+
+
+
+##================================================================================================================##
+#--------------------------------------------------------------------------------------------------#
+# To extract TCGA Breast expression  1246
+#--------------------------------------------------------------------------------------------------#
+library("rhdf5")
+library("preprocessCore")
+
+base_dir <- "/media/cytogenbi2/6eaf3ba8-a866-4e8a-97ef-23c61f7da612/BreastCancer/data/ReCount2OverARCHS4"
+setwd(base_dir)
+samples <- list.files(base_dir)
+
+destination_file = "/media/cytogenbi2/6eaf3ba8-a866-4e8a-97ef-23c61f7da612/BreastCancer/data/ReCount2OverARCHS4/TCGA_matrix.h5"
+
+
+### Extract Columns
+
+genes = h5read(destination_file, "meta/genes")
+#TCGA Barcode
 analytes.submitter_id=h5read(destination_file,"/meta/gdc_cases.samples.portions.analytes.submitter_id")
+
 head(analytes.submitter_id)
+#> head(analytes.submitter_id) #[1] "TCGA-DD-AAVP-01A-11R" "TCGA-KK-A7B2-01A-12R" "TCGA-DC-6158-01A-11R"
 
-> head(analytes.submitter_id)
-[1] "TCGA-DD-AAVP-01A-11R" "TCGA-KK-A7B2-01A-12R" "TCGA-DC-6158-01A-11R"
-[4] "TCGA-DD-A4NP-01A-11R" "TCGA-HQ-A5ND-01A-11R" "TCGA-HT-A614-01A-11R"
 
-#breast Tissue extract
+#breast Primarysite extract
 Tprimarysite=h5read(destination_file,"/meta/gdc_cases.project.primary_site")
-#> head(Tprimarysite)
-#[1] "Liver"      "Prostate"   "Colorectal" "Liver"      "Bladder"   
-#[6] "Brain"   
-Breast 1246
+#> head(Tprimarysite)  #[1] "Liver"      "Prostate"   "Colorectal" "Liver"      "Bladder"   
+
+
 site_locations = which(Tprimarysite %in% "Breast")
 length(site_locations)
 
 expression = h5read(destination_file, "data/expression", index=list(1:length(genes), site_locations))
-#> head(Tcaseid)
-#[1] "0004d251-3f70-4395-b175-c94c2f5b1b81"
-#[2] "000d566c-96c7-4f1c-b36e-fa2222467983"
-#[3] "0011a67b-1ba9-4a32-a6b8-7850759a38cf"
+
 rownames(expression) = genes
 colnames(expression) = analytes.submitter_id[site_locations]
 
+### log2 normalization
 expression=log2(expression+1)
+
+### write csv
 write.csv(expression,file="/media/cytogenbi2/6eaf3ba8-a866-4e8a-97ef-23c61f7da612/BreastCancer/data/ReCount2OverARCHS4/191203TCGABreastTissue.csv")
 
+#--------------------------------------------------------------------------------------------------#
 ## To get "Tprimarysite != Breast" Expression Table
+#--------------------------------------------------------------------------------------------------#
+
 `%notin%` <- Negate(`%in%`)
 ExBreast_locations = which(Tprimarysite %notin% "Breast")
-all_locations = which(Tprimarysite)
 
-> length(ExBreast_locations)
-[1] 10038
-
-> length(Tprimarysite)
-[1] 11284
+length(ExBreast_locations) # Except Breast sample number [1] 10038
+length(Tprimarysite) # Total sample number [1] 11284
 
 analytes.submitter_id[ExBreast_locations]
 table(Tprimarysite[ExBreast_locations])
@@ -100,7 +111,7 @@ Adrenal Gland     Bile Duct       Bladder   Bone Marrow         Brain
       Stomach        Testis        Thymus       Thyroid        Uterus 
           453           156           122           572           646 
 
-
+##================================================================================================================##
 # Identify columns to be extracted
 samples = h5read(destination_file, "meta/sample")
 
