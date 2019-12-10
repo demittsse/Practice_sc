c02c8fff984817054593fe9ffa656eeb364bcdf0
demittsse <somrsaltt@gmail.com>
2019-12-10 18:01:59 +0900

    Update PC2_make_runStarRSEMGRCh38.97.py

--- BI_left/python/PC2_make_runStarRSEMGRCh38.97.py
+++ BI_left/python/PC2_make_runStarRSEMGRCh38.97.py
@@ -3,13 +3,168 @@ import os, glob, time
 start=time.strftime("%y%m%d")
 project_name="07SMC009_%s"%(start)
 
+maindir="/media/cytogenbi2/8e7f6c8b-bc45-4c58-816f-a062fd95b91a/"
 
+newTrimDir="%s02trim/%s"%(maindir,project_name)
+newQCdir="%s01QC/%s"%(maindir,project_name)
+newbeforeQCdir="%s01QC/%s/beforeQC"%(maindir,project_name)
+newafterQCdir="%s01QC/%s/afterQC"%(maindir,project_name)
+newStarDir="%s03star/%s"%(maindir,project_name)
+newQualiDir="%s03qualimap/%s"%(maindir,project_name)
+newRsemDir="%s04rsem/%s"%(maindir,project_name)
+newLogDir="%s04rsem/log_%s"%(maindir,project_name)
+
+if os.path.isdir(newTrimDir) == 0:
+	os.system("mkdir %s"%(newTrimDir))
+
+if os.path.isdir(newQCdir) == 0:
+	os.system("mkdir %s"%(newQCdir))
+
+if os.path.isdir(newbeforeQCdir) == 0:
+	os.system("mkdir %s"%(newbeforeQCdir))
+
+if os.path.isdir(newafterQCdir) == 0:
+	os.system("mkdir %s"%(newafterQCdir))
+
+if os.path.isdir(newStarDir) == 0:
+	os.system("mkdir %s"%(newStarDir))
+
+if os.path.isdir(newQualiDir) == 0:
+	os.system("mkdir %s"%(newQualiDir))
+
+if os.path.isdir(newRsemDir) == 0:
+	os.system("mkdir %s"%(newRsemDir))
+
+if os.path.isdir(newLogDir) == 0:
+	os.system("mkdir %s"%(newLogDir))
+
+##Create ouput file
+import os, glob, time
+start=time.strftime("%y%m%d")
+project_name="07SMC009_%s"%(start)
+
+oufname="%s00script/run/trim%s.sh"%(maindir,project_name)
+ouf=open(oufname,"w")
+
+infFa=glob.glob("/media/cytogenbi1/D2C67EE7C67ECAED/BI/02raw/SMC008_12ea/*_1.fastq.gz")
+
+lineNum=0
+for infile in infFa:
+	lineNum+=1
+	#if lineNum == 1: continue
+	print("#",lineNum)	
+	if "fastq.gz" in infile:
+		fastq2= infile.split("_1.fastq.gz")[0]+"_2.fastq.gz"
+	else :
+		fastq2= infile.split("_1.fastq")[0]+"_2.fastq"
+	cm_trim = "trim_galore %s %s --paired --phred33  -o %s\n\n\n"%(infile,fastq2,newTrimDir)
+    print(cm_trim)
+    ouf.write("#%s\n"%lineNum)
+    ouf.write(cm_trim)
+
+
+## before trim qc
+import os, glob, time
+start=time.strftime("%y%m%d")
+project_name="07SMC009_%s"%(start)
+#project_name="07SMC009_191209"
+maindir="/media/cytogenbi2/8e7f6c8b-bc45-4c58-816f-a062fd95b91a/"
+
+newQCdir="%s01QC/%s"%(maindir,project_name)
+newbeforeQCdir="%s01QC/%s/beforeQC"%(maindir,project_name)
+newafterQCdir="%s01QC/%s/afterQC"%(maindir,project_name)
 ### import input file ###
 #glob raw fastq file - .gz or not
 
 #infFa=glob.glob("/home/cytogen-bi2/01raw/CMC-011_C1_32ea_190624/*_1.fastq.gz")
-infFa=glob.glob("/home/cytogen-bi2/01raw/191001_CMC018_019_24eaPairedEnd_48fastq/*_1.fastq")
 
+rawFa=glob.glob("/media/cytogenbi2/6eaf3ba8-a866-4e8a-97ef-23c61f7da612/01raw/SMC009_30ea/*_1.fastq.gz")
+
+oufname="%s00script/run/runbeforeQC%s.sh"%(maindir,project_name)
+ouf=open(oufname,"w")
+	 
+lineNum=0
+for tfile in rawFa:
+	lineNum+=1
+	#if lineNum == 1: continue
+	print("#",lineNum)	
+	if "fastq.gz" in tfile:
+		fastq2= tfile.split("_1.fastq.gz")[0]+"_2.fastq.gz"
+	else :
+		fastq2= tfile.split("_1.fastq")[0]+"_2.fastq"
+	
+	#trfq2 = tfile.replace("_1_val_1.fq.gz","_2_val_2.fq.gz")
+	cmtr="fastqc -o %s %s %s\n\n"%(newbeforeQCdir, tfile, fastq2)
+	print(cmtr)
+	ouf.write(cmtr)
+
+ouf.close()
+
+os.system("chmod 755 %s"%(oufname))
+
+print("tail %s"%(oufname))
+
+
+## after trim qc
+import os, glob, time
+start=time.strftime("%y%m%d")
+project_name="07SMC009_%s"%(start)
+
+#project_name="07SMC009_191209"
+maindir="/media/cytogenbi2/8e7f6c8b-bc45-4c58-816f-a062fd95b91a/"
+
+newQCdir="%s01QC/%s"%(maindir,project_name)
+newbeforeQCdir="%s01QC/%s/beforeQC"%(maindir,project_name)
+newafterQCdir="%s01QC/%s/afterQC"%(maindir,project_name)
+
+trimFa=glob.glob("%s/02trim/07SMC009_191209/*_1_val_1.fq.gz"%maindir)
+oufname="%s00script/run/runafterQC%s.sh"%(maindir,project_name)
+ouf=open(oufname,"w")
+
+lineNum=0
+for tfile in trimFa:
+	lineNum+=1
+	#if lineNum == 1: continue
+	print("#",lineNum)	
+	fastq2 = tfile.replace("_1_val_1.fq.gz","_2_val_2.fq.gz")
+	cmtr="fastqc -o %s %s %s\n\n"%(newafterQCdir, tfile, fastq2)
+	print(cmtr)
+	ouf.write(cmtr)
+
+ouf.close()
+
+os.system("chmod 755 %s"%(oufname))
+
+print("tail %s"%(oufname))
+
+
+
+# for file in `ls *.zip`; do unzip "${file}" -d "${file:0:-4}"; done
+
+
+qcf =glob.glob("/media/desktop-bi-16/D2C67EE7C67ECAED/BI/03qc/*/*/summary.txt")
+qcline=[]
+for files in qcf:
+	qcff=open(files)
+	qcline.append(qcff.readlines())
+
+failD={}
+qctotal =[]
+for aa in qcline:
+	for bb in aa:
+		qctotal.append(bb)
+			if "fail" in bb:
+			
+		break
+ouf=open("/media/desktop-bi-16/D2C67EE7C67ECAED/BI/03qc/resQC190917.txt","w")
+for aaa in qctotal:
+	if "FAIL" in aaa:
+		wrine = "%s:%s\n"%(aaa.split("\t")[2].strip("\n"),aaa.split("\t")[1])
+		print aaa.split("\t")[2].strip("\n"),":",aaa.split("\t")[1]
+		ouf.write(wrine)
+ouf.close()
+		failD[aaa.split("\t")[2]]=aaa.split("\t")[1]
+	
 #trimmed
 
 trim1=glob.glob("%s/03trim/01CMC1113/*_1_val_1.fq.gz"%maindir)
@@ -81,10 +236,8 @@ if os.path.isdir(newLogDir) == 0:
 	os.system("mkdir %s"%(newLogDir))
 
 
-
-
 #================================================================================================================
-### Commands of Trimming with Trim_galore, Mapping with Star ###
+### Commands of Mapping with Star ###
 #================================================================================================================
 import os, glob, time
 start=time.strftime("%y%m%d")
