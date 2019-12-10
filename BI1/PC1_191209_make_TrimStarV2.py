88d84c59ed4879e468eaebd6a4532df8fc3c16ea
demittsse <somrsaltt@gmail.com>
2019-12-10 10:46:18 +0900

    Update PC1_191209_make_TrimStarV2.py

--- BI1/PC1_191209_make_TrimStarV2.py
+++ BI1/PC1_191209_make_TrimStarV2.py
@@ -25,6 +25,9 @@ if os.path.isdir(newLogDir) == 0:
 	os.system("mkdir %s"%(newLogDir))
 
 ##Create ouput file
+import os, glob, time
+start=time.strftime("%y%m%d")
+project_name="07SMC009_%s"%(start)
 
 oufname="%s00script/run/trim%s.sh"%(maindir,project_name)
 ouf=open(oufname,"w")
@@ -46,50 +49,62 @@ for infile in infFa:
     ouf.write(cm_trim)
 
 #================================================================================================================
-"""
-#=============================================
-## trimming & Star mapping Gencode Ref
-#=============================================
-##fastq.gz
+### Mapping with Star, RSEM Quantification ###
+#================================================================================================================
+
+trimFa=glob.glob("%s/02trim/07SMC009_191209/*_1_val_1.fq.gz"%maindir)
+print("trimFa length = ", len(trimFa))
+
+maindir="/media/cytogenbi1/D2C67EE7C67ECAED/"
+newTrimDir="%s02trim/%s"%(maindir,project_name)
+newStarDir="%s03star/%s"%(maindir,project_name)
+newQualiDir="%s03qualimap/%s"%(maindir,project_name)
+newRsemDir="%s04rsem/%s"%(maindir,project_name)
+newLogDir="%s04rsem/log_%s"%(maindir,project_name)
+
+genomeDir="%s00ref/RSEM_STAR38"%(maindir)
+sjdbGTFfile="%s00ref/ensembl38.97/Homo_sapiens.GRCh38.97.gtf"%(maindir)
+rsemrefDir="%s00ref/RSEM_STAR38"%(maindir)
+
+##Create ouput file
 import os, glob, time
 start=time.strftime("%y%m%d")
-project_name="05CMC018_019"
+project_name="07SMC009_%s"%(start)
+oufname="%s00script/run/starrsem%s.sh"%(maindir,project_name)
+ouf=open(oufname,"w")
 
-### import input file ###
-#infFa=glob.glob("/home/cytogen-bi2/01raw/CMC-011_C1_32ea_190624/*_1.fastq.gz")
-infFa=glob.glob("/home/cytogen-bi2/01raw/191001_CMC018_019_24eaPairedEnd_48fastq/*_1.fastq")
-
-### Create output folder & executable bash file ###
-
-os.system("chmod 777 /home/cytogen-bi2/00script/run/%sTrimStar%s.sh"%(start,project_name))
-newTrimDir="/home/cytogen-bi2/02trim/%s"%(project_name)
-newStarDir="/home/cytogen-bi2/03star/%s"%(project_name)
-
-if os.path.isdir(newTrimDir) == 0:
-	os.system("mkdir %s"%(newTrimDir))
-elif os.path.isdir(newStarDir) == 0:
-	os.system("mkdir %s"%(newStarDir))
-
-ouf=open("/home/cytogen-bi2/00script/run/%sTrimStar%s.sh"%(start,project_name),"w")
-
-### Commands of Trimming with Trim_galore, Mapping with Star ###
-for infile in infFa:
-	if "fastq.gz" in infile:
-		fastq2= infile.split("_1.fastq.gz")[0]+"_2.fastq.gz"
-	else :
-		fastq2= infile.split("_1.fastq")[0]+"_2.fastq"
-	cm_trim = "/home/cytogen-bi2/00source/TrimGalore-0.6.4/trim_galore %s %s --paired --phred33  -o /home/cytogen-bi2/02trim/%s\n\n\n"%(infile,fastq2,project_name)
-	print cm_trim
-	fq=infile.split("/")[-1].split("_1.fastq.gz")[0]
-	print fq
-	cm_salmon="salmon quant -i /home/cytogen-bi2/00ref/salmon/Homo_sapiens.GRCh37.75_quasi_index -l A -1 /home/cytogen-bi2/02trim/%s/%s_1_val_1.fq.gz -2 /home/cytogen-bi2/02trim/%s/%s_2_val_2.fq.gz -p 8 --validateMappings -o %s\n\n\n" %(project_name,fq,project_name,fq,fq)
+lineNum=0
+for infile in trimFa:
+	lineNum+=1
+	#if lineNum == 1: continue
+	print("#",lineNum)	
 	trfq2 = infile.replace("_1_val_1.fq.gz","_2_val_2.fq.gz")
-	cm_star="STAR --genomeDir /home/cytogen-bi2/00ref/gencode --readFilesIn %s,%s --outFileNamePrefix /home/cytogen-bi2/03star/%s/%s  --runThreadN 1 --outSAMtype BAM SortedByCoordinate --readFilesCommand gunzip -c\n\n\n"%(infile, trfq2, project_name, infile.split("/")[-1].split("_1_val_1.fq.gz")[0].split("_")[0])
-	print cm_star
-	ouf.write(cm_trim)
-	ouf.write(cm_star)
-	#ouf.write(cm_salmon)
-	#print cm_salmon
+	sample = infile.split("/")[-1].split("_1_val_1.fq.gz")[0]
+	star_out_file="%s/%s"%(newStarDir, sample)
+	quali_out_dir="%s/%s"%(newQualiDir, sample)
+	rsem_out_file="%s/%s"%(newRsemDir, sample)
+	cm_trimmed_star="STAR --runThreadN 2 --genomeDir %s --sjdbGTFfile %s --sjdbOverhang 150 --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.02 --alignIntronMin 20 --outFileNamePrefix %s --alignIntronMax 1000000 --alignMatesGapMax 1000000 --readFilesIn %s,%s --readFilesCommand zcat --quantMode TranscriptomeSAM\n\n\n"%(str(genomeDir), str(sjdbGTFfile), star_out_file, infile, trfq2)
+	cm_qualimap="qualimap rnaseq -outdir %s -a proportional -bam %sAligned.out.sam -gtf %s --java-mem-size=8G \n\n\n"%(quali_out_dir, star_out_file,sjdbGTFfile)
+	cm_rsem="rsem-calculate-expression --bam --no-bam-output --single-cell-prior -p 1 %sAligned.toTranscriptome.out.bam %s %sscQuant> %sscrsem.log\n\n\n"%(star_out_file, str(rsemrefDir), rsem_out_file, rsem_out_file)
+	print(cm_trimmed_star)
+	print(cm_qualimap)
+	print(cm_rsem)
+	ouf.write(cm_trimmed_star)
+	ouf.write("echo %s %s star alignment done!\n"%(project_name, sample))
+	ouf.write(cm_qualimap)
+	ouf.write("echo %s %s Qualimap done!\n"%(project_name, sample))
+	ouf.write(cm_rsem)
+	ouf.write("echo %s %s rsem done!\n"%(project_name, sample))
 
 ouf.close()
-"""
+
+
+ouf.close()
+
+os.system("chmod 755 %s"%(oufname))
+
+print("tail %s"%(oufname))
+print("%s>%s/%.log"%(oufname,newLogDir, project_name))
+
+print("%s>%s/%.log"%(oufname,newLogDir, project_name))
+#================================================================================================================
