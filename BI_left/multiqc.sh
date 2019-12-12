2d0cc673642fc225665af21e676e6f9208354496
demittsse <somrsaltt@gmail.com>
2019-12-12 16:05:47 +0900

    Create multiqc.sh

---
+++ BI_left/multiqc.sh
@@ -0,0 +1,5 @@
+multiqc /media/cytogenbi2/8e7f6c8b-bc45-4c58-816f-a062fd95b91a/01QC/07SMC008_191212/beforeQC
+multiqc /media/cytogenbi2/8e7f6c8b-bc45-4c58-816f-a062fd95b91a/01QC/07SMC008_191212/afterQC
+multiqc /media/cytogenbi2/8e7f6c8b-bc45-4c58-816f-a062fd95b91a/03star/07SMC008_191212
+multiqc /media/cytogenbi2/8e7f6c8b-bc45-4c58-816f-a062fd95b91a/04rsem/07SMC008_191212
+multiqc /media/cytogenbi2/8e7f6c8b-bc45-4c58-816f-a062fd95b91a/03qualimap/07SMC008_191212
