8558b53576ef30bbcdb8056b6559434bb715028f
demittsse <somrsaltt@gmail.com>
2020-01-08 15:22:59 +0900

    Update run10X.sh

--- 10X/run10X.sh
+++ 10X/run10X.sh
@@ -1,4 +1,4 @@
 cellranger count --id=run_count_10X_009 \
---fastqs=/media/cytogenbi2/6eaf3ba8-a866-4e8a-97ef-23c61f7da612/01raw/HN00119076_10X/HN00119076_10X_RawData_Outs/10X_009/H72NHCCX2 \
+--fastqs=/media/cytogenbi2/8e7f6c8b-bc45-4c58-816f-a062fd95b91a/10X/HN00119076_10X/HN00119076_10X_RawData_Outs/10X_009/H72NHCCX2 \
 --sample=10X_009 \
 --transcriptome=/home/cytogenbi2/00source/refdata-cellranger-GRCh38-3.0.0
