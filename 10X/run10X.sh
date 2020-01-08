d0442afc130ee4bda51c6812ac05092997bf73b0
demittsse <somrsaltt@gmail.com>
2020-01-08 14:34:11 +0900

    Create run10X.sh

---
+++ 10X/run10X.sh
@@ -0,0 +1,4 @@
+cellranger count --id=run_count_10X_009 \
+--fastqs=/media/cytogenbi2/6eaf3ba8-a866-4e8a-97ef-23c61f7da612/01raw/HN00119076_10X/HN00119076_10X_RawData_Outs/10X_009/H72NHCCX2 \
+--sample=10X_009 \
+--transcriptome=/home/cytogenbi2/00source/refdata-cellranger-GRCh38-3.0.0
